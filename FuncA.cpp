ae177233cbe1f9c7ae66d36f078a9708a0e230a4
Bogdan <bogdanremuga@gmail.com>
2024-10-25 00:01:33 +0300

    Update FuncA to calculate first 3 elements of the sequence

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,5 +1,11 @@
-#include "FuncA.h"
-
 double FuncA::calculateFuncA(int n) {
-    return 0.0;
+    double x = 1;
+    double sum = 0.0;
+
+    for (int i = 0; i < 3; ++i) {
+        double term = pow(-1, i) * pow(x, 2 * i + 1) / (2 * i + 1);
+        sum += term;
+    }
+
+    return sum;
 }
