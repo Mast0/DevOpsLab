cc0f174f9b902c756d244ac3622b8f930292e70d
Bogdan <bogdanremuga@gmail.com>
2024-10-24 23:46:32 +0300

    Initial commit with FuncA class returning 0

---
+++ FuncA.h
@@ -0,0 +1,9 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+class FuncA {
+public:
+    double calculateFuncA(int n);
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+    FuncA func;
+    std::cout << "FuncA result: " << func.calculateFuncA(0) << std::endl;
+    return 0;
+}
